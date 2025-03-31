163434fc4f11f7345355df93557815df7b7093a5
g <pidgions21@gmail.com>
2025-03-31 11:09:13 -0400

    appeased da linter

--- app/transmitter/transmitter.py
+++ app/transmitter/transmitter.py
@@ -1,25 +1,30 @@
 import threading
-import time
-import socket
-from scapy.all import *
 import random
+import string
+from scapy.layers.dot11 import RadioTap, Dot11, LLC, SNAP
+from scapy.all import sendp, RandMAC, Raw
+
 
 class Transmitter(threading.Thread):
     def __init__(self, target_mac='ff:ff:ff:ff:ff:ff', interface='wlp0s20f3'):
         super().__init__()
-        self._stay_alive  = threading.Event()
-        self.interface     = interface
-        self.target_mac   = target_mac
+        self._stay_alive = threading.Event()
+        self.interface = interface
+        self.target_mac = target_mac
+        self._n_packets_sent = 0
 
     def send_frame(self, payload):
         frame = RadioTap() / Dot11(
-            type    = 2,
-            subtype = 0,
-            addr1   = self.target_mac,
-            addr2   = RandMAC(),
-            addr3   = self.target_mac
+            type=2,
+            subtype=0,
+            addr1=self.target_mac,
+            addr2=RandMAC(),
+            addr3=self.target_mac
         ) / LLC() / SNAP() / Raw(load=payload)
         sendp(frame, iface=self.interface, verbose=False)
+
+    def get_n_packets_sent(self):
+        return self._n_packets_sent
 
     def set_target_mac(self, mac):
         self.target_mac = mac
@@ -31,6 +36,7 @@ class Transmitter(threading.Thread):
                 data_sz = 50
                 data = ''.join(random.choices(string.printable, k=data_sz)).encode()
                 self.send_frame(data)
+                self._n_packets_sent += 1
         except KeyboardInterrupt:
             print(f'Keyboard interrupt in {self.__class__.__name__}'.upper())
             self.kill()
